332aa6e137b431aa7ff2828f8d06e965f0229b2e
Aron Rohr <aron.rohr@web.de>
2023-01-06 14:46:27 +0100

    update

--- main.py
+++ main.py
@@ -13,39 +13,78 @@ def loadDir(path):
 
 
 def checkFile(path, file):
-    if not file.endswith(".lua") or (not file.startswith("sv_") and not file == "init.lua" and 'server' not in path):
+    if not file.endswith(".lua"):
         return
-    f = open(path + "/" + file)
-    content = f.read()
-    attention = False
-    net_receives = []
-    for line in content.splitlines():
-        if attention:
-            net_receives[len(net_receives) - 1].append(line)
-            if line == 'end)':
-                attention = False
-        if 'net.Receive' in line:
-            attention = True
-            net_receives.append([])
-            net_receives[len(net_receives) - 1].append(line)
-    net_receives = formatArray(net_receives)
-    for receive in net_receives:
-        is_exploitable = check_net_receive(receive)
-        if not is_exploitable:
-            print("Not exploitable: " + re.search(r'\"(.+?)\"', receive[0]).group(1))
-            continue
-        print("Possible exploitable net message: " + re.search(r'\"(.+?)\"', receive[0]).group(1))
+    if file.startswith("sv_") or file == "init.lua" or 'server' in path:
+        f = open(path + "/" + file)
+        content = f.read()
+        attention = False
+        net_receives = []
+        for line in content.splitlines():
+            if attention:
+                net_receives[len(net_receives) - 1].append(line)
+                if line == 'end)':
+                    attention = False
+            if 'net.Receive' in line:
+                attention = True
+                net_receives.append([])
+                net_receives[len(net_receives) - 1].append(line)
+        net_receives = formatArray(net_receives)
+        for receive in net_receives:
+            is_exploitable, exploit = check_net_receive(receive)
+            if not is_exploitable:
+                print("Not exploitable: " + re.search(r'\"(.+?)\"', receive[0]).group(1))
+                continue
+            print("Possible server side exploitable net message: " + re.search(r'\"(.+?)\"', receive[0]).group(1) + "\nReason: " + exploit)
+    elif file.startswith("cl_") or 'client' in path:
+        f = open(path + "/" + file)
+        content = f.read()
+        attention = False
+        net_sending = []
+        for line in content.splitlines():
+            if attention:
+                net_sending[len(net_sending) - 1].append(line)
+                if line == 'net.SendToServer()':
+                    attention = False
+            if 'net.Start' in line:
+                attention = True
+                net_sending.append([])
+                net_sending[len(net_sending) - 1].append(line)
+        net_sending = formatArray(net_sending)
+        for send in net_sending:
+            is_exploitable, exploit = check_net_send(send)
+            if not is_exploitable:
+                print("Not exploitable: " + re.search(r'\"(.+?)\"', send[0]).group(1))
+                continue
+            print("Possible server side exploitable net message: " + re.search(r'\"(.+?)\"', send[0]).group(1) + "\nReason: " + exploit)
+
+
+def check_net_send(net_send):
+    is_exploitable = False
+    exploit = ""
+    local_player_var = ""
+    for line in net_send:
+        if 'net.WriteEntity' in line and 'LocalPlayer()' in line:
+            is_exploitable = True
+            exploit = "LocalPlayer getting send"
 
 
 def check_net_receive(net_receive):
     is_exploitable = False
-    exploit_list = []
+    contains_if = False
+    exploit = ""
     for line in net_receive:
         if 'if' in line:
-            if 'GetUserGroup' not in line or 'getJobTable' not in line or 'Trace' not in line:
+            contains_if = True
+            if 'GetUserGroup' not in line or 'getJobTable' not in line or 'Trace' not in line or 'getDarkRPVar' not in line:
+                exploit = "No check of permissions"
                 is_exploitable = True
 
-    return is_exploitable
+    if not contains_if:
+        exploit = "No check of anything"
+        is_exploitable = True
+
+    return is_exploitable, exploit
 
 
 def formatArray(big_array):
